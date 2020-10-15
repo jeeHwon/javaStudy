fe0f9eccdbd72d701d55dfa6d267c57d69f6e6d4
jeeHwon <jeehwon01@gmail.com>
2020-10-15 17:55:05 +0900

    jdbc modi

--- src/jdbc/JdbcTest.java
+++ src/jdbc/JdbcTest.java
@@ -18,6 +18,7 @@ public class JdbcTest {
 		String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		String user = "scott";
 		String password = "tiger";
+		
 		Connection conn = null;
 		Statement stmt = null;
 		//1. JDBC 드라이버 로드
@@ -39,7 +40,6 @@ public class JdbcTest {
 		//DriverManager.getConnection() 메소드를 호출하여
 		//데이터베이스에 연결하고 Connection 객체를 반환한다
 		//DriverManager.getConnection(url, user, password)
-		
 		try {
 			conn = DriverManager.getConnection(url,user,password);
 			System.out.println("connection 얻기 성공");
@@ -51,20 +51,8 @@ public class JdbcTest {
 		//자바에서 SQL 문을 실행하기 위해서는 Statment 클래스
 		try {
 			stmt = conn.createStatement();
-		} catch (Exception e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 	}
-
 }
