e89904497ee34986ecc6eb887a8a9ccd0530a648
jeeHwon <jeehwon01@gmail.com>
2020-10-18 14:02:16 +0900

    Q3 ver2

--- src/Q3/BoardDTO.java
+++ src/Q3/BoardDTO.java
@@ -1,5 +1,33 @@
 package Q3;
 
 public class BoardDTO {
-
+	//title과 content 선언 field
+	//private 써서 다른 클래스에서 접근 못하게 방어!
+	private String title;
+	private String content;
+	
+	//constructor에서 title, content받아 객체 생성하게끔 강제
+	public BoardDTO(String title, String content){
+		this.title = title;
+		this.content = content;
+	}
+	
+	//setter/getter method 구성
+	public String getTitle() {
+		return title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	//toString override
+	@Override
+	public String toString() {
+		return "BoardDTO [title= "+title+", content ="+content+"]";
+	}
 }
--- src/Q3/BoardListExample.java
+++ src/Q3/BoardListExample.java
@@ -2,4 +2,21 @@ package Q3;
 
 public class BoardListExample {
 
+	public static void main(String[] args) {
+		//DAO 멤버 호출하기 위해 DAO 객체 생성
+		BoardDAO dao = new BoardDAO();
+		
+		//DAO의 method 이용해서 제목, 내용을 받아 dtos 리스트에 DTO로 형태로 CRUD
+		//게시글 등록
+		dao.insert("제목1", "내용1");
+		dao.insert("제목2", "내용2");
+		dao.insert("제목3", "내용33");
+		dao.insert("제목4", "내용4");
+		//게시글 삭제
+		dao.delete("제목4");
+		//게시글 수정
+		dao.update("제목3", "내용3");
+		//게시글 조회
+		dao.select();
+	}
 }
